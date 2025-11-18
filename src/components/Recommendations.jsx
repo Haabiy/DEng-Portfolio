50981b501aa02eb114e9a79b72dc28bff44c9be7
Abiy <abiy.dema@gmail.com>
2025-11-18 10:10:32 +0100

    feat: added month

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,14 +1,14 @@
 import React, {useEffect, useRef} from 'react';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
-const RecommendationCard = ({ quote, name, position, year }) => (
+const RecommendationCard = ({ quote, name, position, month, year }) => (
   <div className="bg-gray-800 p-6 rounded-lg shadow-lg transition-all duration-300 hover:shadow-xl hover:shadow-blue-400/30 hover:-translate-y-1">
     <FaQuoteLeft className="text-blue-400 text-2xl mb-4" />
     <p className="text-gray-300 mb-4 min-h-[150px]">{quote}</p>
     <div className="mt-auto">
       <h5 className="text-lg font-semibold text-white">{name}</h5>
       <p className="text-sm text-gray-400">{position}</p>
-      <p className="text-sm text-gray-400">© {year}</p>
+      <p className="text-sm text-gray-400">{month} {year}</p>
     </div>
     <FaQuoteRight className="text-blue-400 text-2xl mt-4 float-right" />
   </div>
@@ -28,24 +28,28 @@ const Recommendations = () => {
       quote: "I had the pleasure of managing Abiy as his team lead. Abiy is a motivated data engineer known for his quick learning and readiness to tackle challenges. His diverse skill set, ranging from Python and SQL to AWS data stack, showcases his adaptability and eagerness to learn and grow in any fast-paced environment.",
       name: "Hammad Aslam KHAN",
       position: "Data Engineering & Architecture at Mangabey",
+      month: "May",
       year: "2024"
     },
     {
       quote: "I had the pleasure of working with Abiy, a highly skilled Data Engineer who excelled in our fast-paced environment. His adaptability and ability to simplify complex issues made him indispensable during high-pressure situations. Abiy's collaborative spirit and knack for making data accessible significantly boosted our project success.",
       name: "Christelle Depierre-Barrault",
       position: "Program Director at Mangabey",
+      month: "October",
       year: "2023"
     },
     {
       quote: "We have been lucky to have Abiy on the Fifteen marketing team for his final year internship, where he successfully helped us identify more leads, and qualify them, using data scraping techniques. Abiy is easy to work with, he has good technical skills and a relentless motivation to learn new things.",
       name: "Damien Brunet",
       position: "CMO at Fifteen",
+      month: "September",
       year: "2022"
     },
     {
       quote: "Abiy produced stellar work throughout his internship, be it web-scraping complex websites or wrangling data to deliver value. He is very curious, and showed himself eager to learn new tools and technique to apply to his tasks. It has been a pleasure working and exchanging with him!",
       name: "Francesco Cusimano",
       position: "Data Scientist at Fifteen",
+      month: "August",
       year: "2022"
     },
   ];
